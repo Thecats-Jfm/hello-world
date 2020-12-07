daeeaf5e626199a06fe62716a57ab6c4b82006fb
Thecats <Thecats_Jfm@outlook.com>
2020-12-07 16:38:31 +0800

    yeah!

--- lab5.py
+++ lab5.py
@@ -1,46 +1,5 @@
-import random
-import time
-num_list= [random.randint(1,100000) for i in range(100000)]
-
-'''顺序检索
-时间复杂度：O(n)
-优点：数据存储可以无序，插入元素可以直接加在表尾
-缺点：检索时间长
-'''
-def direct_search(key):
-    for i in range(len(num_list)):
-        if(num_list[i] == key):
-            print(i)
-
-'''二分检索
-时间复杂度：O(log(n))
-优点：比较次数少，检索速度快
-缺点：需要按照关键码排序，不易更新
-'''
-def binarySearch(key):
-    low,high = 0,len(num_list)-1
-    while(low<=high):
-        mid = (low+high)//2
-        if num_list[mid]==key:
-            print("找到目标，索引号是:",mid)
-            break
-        else:
-            if num_list[mid]<key:
-                low,high =mid+1,high
-            else:
-                low,high =low,mid-1
-    else:
-        print("未找到")
-
-start1 = time.time()
-for i in range(100):
-    direct_search(random.randint(1,100000))
-end1  = time.time()
-num_list.sort()
-start2 = time.time()
-for i in range(100):
-    binarySearch(random.randint(1,100000))
-end2  = time.time()
-print("\n在长度为100000，取值为1~100000的序列中：")
-print("100次直接检索用时",end1-start1,"秒")
-print("100次二分法检索用时",end2-start2,"秒")
+l= '{"status":0,":1}],"data":""}'
+import re
+s = l.find('"num')
+j = l.find(',"voted')
+print(l[s:j])
--- t.cpp
+++ t.cpp
@@ -1,22 +1,26 @@
-class Answer{
-public:
-    int *B;
-    int Sort(int *A,int l,int r){
-        if(l==r) return 0;
-        int m = (l+r)/2;
-        int pos1=l,pos2=m+1,ret=Sort(A,l,m)+Sort(A,m+1,r),pos=l;
-        while(pos1<=m&&pos2<=r){
-            if(A[pos1]<=A[pos2]){
-                B[pos++]=A[pos1++];
-            }
-            else{
-                B[pos++]=A[pos2++];
-                ret += m - pos1 +1;
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+    char a[4][260],out[260],c;
+    int i,j,l=260,tp;
+    for(i=0;i<4;i++) {
+        gets(a[i]);
+        tp=strlen(a[i]);
+        if(tp<l) l=tp;
+    }
+
+    for(i=0;i<l;i++){
+        c=a[0][i];
+        for(j=1;j<4;j++){
+            if(a[j][i]!=c){
+                l=0;
+                break;
             }
         }
-        while(pos1<=m) B[pos++]=A[pos1++];
-        while(pos2<=r) B[pos++]=A[pos2++];
-        for(int i=l;i<=r;i++) A[i]=B[i];
-        return ret;
+        if(l!=0) out[i]=c;
     }
+    printf("%s\n",out);
+    return 0;
 }
